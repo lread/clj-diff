63b488e7aa20f45c75d9329da45c6696bc0c13df
Ray Huang <raymond.huang@freelancer.com>
2015-02-02 22:07:20 -0800

    Fixing includes

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
 (defproject clj-diff "1.0.0-SNAPSHOT"
   :description "Sequential diff in Clojure."
   :url "http://github.com/brentonashworth/clj-diff"
-  :source-path "src"
+  :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
+  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
+
   :dev-dependencies [[org.clojure/clojure "1.6.0"]
                      [marginalia "0.8.0"]
                      [lein-difftest "1.3.2-SNAPSHOT"]]
 
-  :prep-tasks [["cljx" "once"]]
+  :prep-tasks [["cljx" "once"] "javac" "compile"]
   :profiles {:dev {:plugins [[com.keminglabs/cljx "0.5.0"]]}}
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/classes"
