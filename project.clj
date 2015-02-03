2d9583c0c3e55b5879983e7c0c3ec7d8194599b0
Ray Huang <raymond.huang@freelancer.com>
2015-02-02 22:07:09 -0800

    Updating project with cljx

--- project.clj
+++ project.clj
@@ -1,13 +1,19 @@
 (defproject clj-diff "1.0.0-SNAPSHOT"
   :description "Sequential diff in Clojure."
   :url "http://github.com/brentonashworth/clj-diff"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :java-fork "true"
-  :java-debug "true"
-  :hooks [leiningen.hooks.javac
-          leiningen.hooks.difftest]
-  :dev-dependencies [[org.clojure/clojure "1.2.0"]
-                     [lein-javac "1.2.1-SNAPSHOT"]
-                     [marginalia "0.5.0"]
-                     [lein-difftest "1.3.2-SNAPSHOT"]])
+  :source-path "src"
+  :java-source-paths ["src/jvm"]
+  :dev-dependencies [[org.clojure/clojure "1.6.0"]
+                     [marginalia "0.8.0"]
+                     [lein-difftest "1.3.2-SNAPSHOT"]]
+
+  :prep-tasks [["cljx" "once"]]
+  :profiles {:dev {:plugins [[com.keminglabs/cljx "0.5.0"]]}}
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :clj}
+
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :cljs}]}
+  )
