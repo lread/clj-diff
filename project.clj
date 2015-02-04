896767f46a4078b4c6f3a0917def056d658cd657
Ray H <rymndhng@gmail.com>
2015-02-03 23:06:33 -0800

    simplified project setup

--- project.clj
+++ project.clj
@@ -1,52 +1,44 @@
 (defproject clj-diff "1.0.0-SNAPSHOT"
   :description "Sequential diff in Clojure."
   :url "http://github.com/brentonashworth/clj-diff"
-  :source-paths ["src/clj"]
+  :source-paths ["src/cljx" "src/clj"]
+  :test-paths ["target/test-classes"]
+  :auto-clean false
   :java-source-paths ["src/jvm"]
-  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
-  :test-paths ["test/clj_diff/test"]
-
-  :prep-tasks [["cljx" "once"] "javac" "compile"]
+  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store|\.class"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]]
 
-  :profiles {:dev {:plugins [[com.keminglabs/cljx "0.5.0"]
-                             [lein-difftest "2.0.0"]]
-                   :source-paths ["test"]
-                   :dependencies [[marginalia "0.8.0"]]}
-             :clj {:source-paths ["src/clj" "target/generated/src/clj"]
-                   :test-paths ["test/clj" "target/generated/test/clj"]}
-             :cljs {:dependencies [[org.clojure/clojurescript "0.0-2760"]]
-                    :plugins [[com.cemerick/clojurescript.test "0.3.3"]
-                              [lein-cljsbuild "1.0.4"]]
-                    :source-paths ["target/generated/src/cljs"
-                                   "target/generated/test/cljs"]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2760"]
+                                  [marginalia "0.8.0"]]
+                   :plugins [[com.keminglabs/cljx "0.5.0"]
+                             [lein-difftest "2.0.0"]
+                             [com.cemerick/clojurescript.test "0.3.3"]
+                             [lein-cljsbuild "1.0.4"]]
+                   :aliases {"cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]
+                             "jar" ["do" "clean," "cljx" "once," "jar"]
+                             "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]}}}
 
   :cljsbuild {:builds
-              {:dev {:source-paths ["target/generated/src/cljs"
-                                    "target/generated/test/cljs"]
-                     :compiler {:output-to "target/tests.js"
+              {:test {:source-paths ["target/classes" "target/test-classes"]
+                      :compiler {:output-to "target/testable.js"
                                 :optimizations :simple
                                 :pretty-print true}}}
-              :test-commands {"test" ["node" :node-runner "target/tests.js"]}}
+              :test-commands {"test" ["node" :node-runner "target/testable.js"]}}
 
   :cljx {:builds [{:source-paths ["src/cljx"]
-                                 :output-path "target/generated/src/clj"
-                                 :rules :clj}
-                                {:source-paths ["src/cljx"]
-                                 :output-path "target/generated/src/cljs"
-                                 :rules :cljs}
-                                {:source-paths ["test/cljx"]
-                                 :output-path "target/generated/test/clj"
-                                 :rules :clj}
-                                {:source-paths ["test/cljx"]
-                                 :output-path "target/generated/test/cljs"
-                                 :rules :cljs}]}
+                   :output-path "target/classes"
+                   :rules :clj}
 
-  :aliases {"clj-test" ["with-profile","clj","test"]
-            "cljs-test" ["do" "cljx," "with-profile" "cljs" "cljsbuild" "test"]
-            "clj-clean-test" ["do" "clean," "clj-test"]
-            "cljs-clean-test" ["do" "clean," "cljs-test"]
-            "all-tests" ["do" "clean," "clj-test," "cljs-test"]}
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :cljs}
 
-)
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/test-classes"
+                   :rules :clj}
+
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/test-classes"
+                   :rules :cljs}]}
+  )
